05f687a780309ad7d5cea252d415d12d92272408
Razac-elda <leo.mazzon97@gmail.com>
2020-07-22 17:42:47 +0200

    Navbar e pagine di prova

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/voli')
+def voli():
+    return render_template('voli.html', title='Voli')
+
+
+@app.route('/notizie')
+def notizie():
+    return render_template('notizie.html', title='Notizie')
+
+
+@app.route('/contatti')
+def contatti():
+    return render_template('contatti.html', title='Contatti')
+
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='Account')
