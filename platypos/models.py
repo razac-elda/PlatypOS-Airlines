41ed2ffa2fab97585d48163360564239be78d853
Razac-elda <leo.mazzon97@gmail.com>
2020-08-19 18:22:44 +0200

    Test concorrenza

--- platypos/models.py
+++ platypos/models.py
@@ -1,14 +1,8 @@
 from sqlalchemy import *
 
-# Scegliere il db da usare, ci sono cose che funzionano diversamente tra i db
-
-# La password è quella che chiede quando si installa postgres, mettere "passwordsupersegreta" per facilitare lo sviluppo
-# "platypos_airlines" è il nome del db da creare da shell o pgadmin4
-# Da fare: pip install psycopg2
 uri = 'postgres+psycopg2://postgres:passwordsupersegreta@localhost:5432/test'
 engine = create_engine(uri, echo=True)
 
-# Per TESTARE in memoria con SQLite, serve il parametro check_same_thread
 # uri = 'sqlite:///:memory:'
 # engine = create_engine(uri, echo=True, connect_args={"check_same_thread": False})
 
