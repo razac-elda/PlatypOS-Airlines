82f3b9f6e6aa5a071eb67b4592822cfcdae69324
alep94 <alepiazza94@hotmail.it>
2020-08-20 11:53:03 +0200

    aggiunta request form della home
    ho aggiunto un commento se volete potete eliminarlo

--- platypos/main/routes.py
+++ platypos/main/routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
 from platypos.models import *
 
 main = Blueprint('main', __name__, template_folder='templates', static_folder='static')
@@ -17,4 +16,18 @@ def homepage():
 
 @main.route('/result', methods=['GET'])
 def search_results():
+    
+    flyFrom = request.form['fly_from']
+    flyTo = request.form['fly_to']
+    #valori possibli di flyght_type sono fromTo e fromOnly
+    flyType = request.form['flight_type']
+    flyDepDate = request.form['fly_dep_date']
+    flyReturnDate = request.form['fly_return_date']
     return render_template('home.html', active_menu=0)
+
+"""connection = engine.connect();
+    if flyType == 'fromOnly':
+    elif flyType == 'fromTo':
+    else:
+    connection.close();
+ """
