ea2f74561067fdb791db04006b1e89fd4205c773
alep94 <alepiazza94@hotmail.it>
2020-08-20 19:13:40 +0200

    inizio della query della home

--- platypos/main/routes.py
+++ platypos/main/routes.py
@@ -14,28 +14,23 @@ def homepage():
     return render_template('home.html', active_menu=0, answer=results)
 
 
-@main.route('/result', methods=['GET'])
+@main.route('/result', methods=['get'])
 def search_results():
-    
-    flyFrom = request.form['fly_from']
-    flyTo = request.form['fly_to']
-    #valori possibli di flyght_type sono fromTo e fromOnly
-    flyType = request.form['flight_type']
-    flyDepDate = request.form['fly_dep_date']
-    flyReturnDate = request.form['fly_return_date']
+    """
+        flyFrom = request.form['fly_from']
+        flyTo = request.form['fly_to']
+        #valori possibli di flyght_type sono fromTo e fromOnly
+        flyType = request.form['flight_type']
+        flyDepDate = request.form['fly_dep_date']
+        flyReturnDate = request.form['fly_return_date']
+    """
+
+    connection = engine.connect()
+    s = text("SELECT  f.flight_code as flight_code ,a1.name as departure_airport , a2.name as arrival_airport , f.departure_time as departure_time, f.arrival_time as arrival_time"
+            " FROM airports a1 JOIN flights f ON a1.airport_id = f.departure_airport JOIN airports a2 ON a2.airport_id = f.arrival_airport")
+    results = connection.execute(s)
+    connection.close()
+
     return render_template('home.html', active_menu=0)
 
-'''
-    connection = engine.connect();
-    if flyType == 'fromOnly':
-        s = text (
-            " SELECT a1.name , a2.name , f.departureTime , f.flightCode "
-            "FROM flighs JOIN  airports a1 ON f.departureAirport = a1.airportId JOIN airports a2 ON a2.airportId = f.arrivalAirporta "
-            "WHERE a1.name = flyFrom AND flyTo = a2.name AND flyType = fromOnly"  
-        )
-        results = connection.execute(s)
-        connection.close();
-
-    return render_template('home.html', active_menu=0, resultsFlights=results)
-'''
 
