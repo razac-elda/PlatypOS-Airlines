9d2dd039413428edfb46d28c686d8bf3b0b2f65f
giulionicola27 <34723867+giulionicola27@users.noreply.github.com>
2020-08-21 15:07:43 +0200

    query home

--- platypos/main/routes.py
+++ platypos/main/routes.py
@@ -14,23 +14,24 @@ def homepage():
     return render_template('home.html', active_menu=0, answer=results)
 
 
-@main.route('/result', methods=['get'])
+@main.route('/result', methods=['GET'])
 def search_results():
-    """
+
         flyFrom = request.form['fly_from']
         flyTo = request.form['fly_to']
         #valori possibli di flyght_type sono fromTo e fromOnly
         flyType = request.form['flight_type']
         flyDepDate = request.form['fly_dep_date']
-        flyReturnDate = request.form['fly_return_date']
-    """
+        if flyType == 'fromTo':
+            flyReturnDate = request.form['fly_return_date']
 
-    connection = engine.connect()
-    s = text("SELECT  f.flight_code as flight_code ,a1.name as departure_airport , a2.name as arrival_airport , f.departure_time as departure_time, f.arrival_time as arrival_time"
-            " FROM airports a1 JOIN flights f ON a1.airport_id = f.departure_airport JOIN airports a2 ON a2.airport_id = f.arrival_airport")
-    results = connection.execute(s)
-    connection.close()
+        connection = engine.connect()
+        s = text("SELECT  f.flight_code as flight_code ,a1.name as departure_airport , a2.name as arrival_airport , f.departure_time as departure_time, f.arrival_time as arrival_time"
+                    " FROM airports a1 JOIN flights f ON a1.airport_id = f.departure_airport JOIN airports a2 ON a2.airport_id = f.arrival_airport"
+                    " WHERE a1.city=:cityfrom AND a2.city=:cityto AND date(f.departure_time)=:departure")
+        results = connection.execute(s, cityfrom=flyFrom, cityto=flyTo, departure=flyDepDate)
+        connection.close()
 
-    return render_template('home.html', active_menu=0)
+        return render_template('home.html', active_menu=0)
 
 
