710b30f69744c3dfddfcb3d109cd40c6ce000f09
alep94 <alepiazza94@hotmail.it>
2020-08-21 15:56:35 +0200

    Aggiustato request e lista

--- platypos/main/routes.py
+++ platypos/main/routes.py
@@ -4,34 +4,28 @@ from platypos.models import *
 main = Blueprint('main', __name__, template_folder='templates', static_folder='static')
 
 
-@main.route('/')
-@main.route('/home')
+@main.route('/', methods=['GET', 'POST'])
+@main.route('/home', methods=['GET', 'POST'])
 def homepage():
     connection = engine.connect()
-
-    results = connection.execute("SELECT distinct city FROM airports ORDER BY city")
+    list = connection.execute("SELECT distinct city FROM airports ORDER BY city")
     connection.close()
-    return render_template('home.html', active_menu=0, answer=results)
 
-
-@main.route('/result', methods=['GET'])
-def search_results():
-
+    if request.method == 'POST':
+        connection = engine.connect()
         flyFrom = request.form['fly_from']
         flyTo = request.form['fly_to']
-        #valori possibli di flyght_type sono fromTo e fromOnly
+        # valori possibli di flyght_type sono fromTo e fromOnly
         flyType = request.form['flight_type']
         flyDepDate = request.form['fly_dep_date']
         if flyType == 'fromTo':
             flyReturnDate = request.form['fly_return_date']
 
-        connection = engine.connect()
-        s = text("SELECT  f.flight_code as flight_code ,a1.name as departure_airport , a2.name as arrival_airport , f.departure_time as departure_time, f.arrival_time as arrival_time"
-                    " FROM airports a1 JOIN flights f ON a1.airport_id = f.departure_airport JOIN airports a2 ON a2.airport_id = f.arrival_airport"
-                    " WHERE a1.city=:cityfrom AND a2.city=:cityto AND date(f.departure_time)=:departure")
+        s = text(
+            "SELECT  f.flight_code as flight_code ,a1.name as departure_airport , a2.name as arrival_airport , f.departure_time as departure_time, f.arrival_time as arrival_time"
+            " FROM airports a1 JOIN flights f ON a1.airport_id = f.departure_airport JOIN airports a2 ON a2.airport_id = f.arrival_airport"
+            " WHERE a1.city=:cityfrom AND a2.city=:cityto AND date(f.departure_time)=:departure")
         results = connection.execute(s, cityfrom=flyFrom, cityto=flyTo, departure=flyDepDate)
         connection.close()
-
-        return render_template('home.html', active_menu=0)
-
-
+        return render_template('home.html', active_menu=0, result=results, dynamiclist=list)
+    return render_template('home.html', active_menu=0, dynamiclist=list)
